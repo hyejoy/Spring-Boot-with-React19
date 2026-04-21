c62b6c6e82738db5c602cd073f61ebe57753c291
hyejoy <devchoi97@gmail.com>
2026-04-21 16:33:50 +0900

    feat: 엔티티 클래스 만들기 - 실행시 DB Table 자동생성법 학습 - JPARepository<entity,PK_Type> 사용

---
+++ src/main/java/org/zerock/mallapi/domain/Todo.java
@@ -0,0 +1,36 @@
+package org.zerock.mallapi.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tbl_todo")
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tno;
+
+    private String title;
+
+    private String writer;
+
+    private boolean complete;
+
+    private LocalDate dueDate;
+}
---
+++ src/main/java/org/zerock/mallapi/repository/TodoRepository.java
@@ -0,0 +1,8 @@
+package org.zerock.mallapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.mallapi.domain.Todo;
+
+// <entity, PK type> 지정
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
