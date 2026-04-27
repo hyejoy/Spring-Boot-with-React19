87bee86a992624741f960eaf2ea19412e7b2c679
hyejoy <devchoi97@gmail.com>
2026-04-27 11:26:59 +0900

    feat: PageResponseDTO 생성 - TodoService 변경예정

--- src/main/java/org/zerock/mallapi/dto/PageReqeusetDTO.java
+++ src/main/java/org/zerock/mallapi/dto/PageReqeusetDTO.java
@@ -1,4 +1,20 @@
 package org.zerock.mallapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder // 상속해서 검색 조건 추가 가능하도록
+@AllArgsConstructor
+@NoArgsConstructor
 public class PageReqeusetDTO {
+
+    @Builder.Default
+    private int page = 1;
+
+    @Builder.Default
+    private int size = 10;
 }
--- src/main/java/org/zerock/mallapi/service/TodoService.java
+++ src/main/java/org/zerock/mallapi/service/TodoService.java
@@ -1,14 +1,15 @@
 package org.zerock.mallapi.service;
 
+import org.zerock.mallapi.dto.PageReqeusetDTO;
+import org.zerock.mallapi.dto.PageResponseDTO;
 import org.zerock.mallapi.dto.TodoDTO;
 
 public interface TodoService {
-    Long register(TodoDTO todoDTO);
-
-    TodoDTO get(Long tno);
-
-
+    Long register(TodoDTO todoDTO);           // 등록
+    TodoDTO get(Long tno);                    // 단건 조회
     // return 타입보다는 예외로 설계
-    void modify(TodoDTO todoDTO);
-    void remove(long tno);
+    void modify(TodoDTO todoDTO);             // 수정
+    void remove(long tno);                    // 삭제 (Sofre Delete 권장
+
+//    PageResponseDTO getList(PageReqeusetDTO pageReqeusetDTO); // 목록
 }
